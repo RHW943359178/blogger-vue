89b2311a81cf0f729fcaaf313736b34cfa6ab27f
RHW943359178 <943359178@qq.com>
2020-11-25 23:14:22 +0800

    文章具体信息页面ui调整

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -18,7 +18,16 @@
             </div>
           </div>
         </div>
-        <div class="content" id="articleInfo_content" v-highlight v-html="articleInfo.content"></div>
+        <!-- <div class="content" id="articleInfo_content" v-highlight v-html="articleInfo.content"></div> -->
+      <mavonEditor
+      class="md"
+     :value="articleInfo.content"
+     :subfield="mavonEditorOption.subfield"
+     :defaultOpen="mavonEditorOption.defaultOpen"
+     :toolbarsFlag="mavonEditorOption.toolbarsFlag"
+     :editable="mavonEditorOption.editable"
+     :scrollStyle="mavonEditorOption.scrollStyle"
+  ></mavonEditor>
       </div>
       <div class="recommend"></div>
     </div>
@@ -26,13 +35,70 @@
 </template>
 
 <script>
+import { mavonEditor } from 'mavon-editor'
 import ARTICLE_DETAIL from '../api/articleDetail'
 import marked from 'marked'
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/monokai-sublime.css";
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: function (code1) {
+    return hljs.highlightAuto(code1).value;
+  },
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false,
+})
+
 export default {
+    components: {
+    mavonEditor
+  },
+  name: "Markdown",
+  props: {
+    data: {
+      type: String,
+      default: "",
+    },
+    lang: {
+      type: String,
+      default: "js",
+    },
+    isMd: {
+      //是否已经是md格式
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    prop () {
+      return {
+        subfield: false,// 单双栏模式
+        defaultOpen: 'preview',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域 
+        editable: false,
+        toolbarsFlag: false,
+        scrollStyle: true
+      }
+      // return data
+    }
+  },
   data() {
     return {
       articleInfo: '',
-      contentLength: 0
+      contentLength: 0,
+      mavonEditorOption: {
+        subfield: false,// 单双栏模式
+        defaultOpen: 'preview',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域 
+        editable: false,
+        toolbarsFlag: false,
+        scrollStyle: true
+      }
     }
   },
   mounted() {
@@ -53,7 +119,8 @@ export default {
         if (result && result.code == 200) {
           this.articleInfo = result.data
           //  使用marked插件将markdown语法渲染成html语法
-          this.articleInfo.content = marked(this.articleInfo.content)
+          // this.articleInfo.content = marked(this.articleInfo.content)
+          this.articleInfo.content = this.articleInfo.content
           this.contentLength = this.articleInfo.content.length
         }
       })
